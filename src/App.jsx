4490f1a66de1c3b215e6906094b778d9b2313569
EdwinSch <edwinschutjes@gmail.com>
2024-03-22 15:54:01 +0100

    prj init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Logo from "./components/Logo";
+
+function App() {
+  return <Logo />;
+}
+
+export default App;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+const Logo = () => {
+  return (
+    <div className="bg-red-700 w-12 h-12 grid place-items-center">
+      <p className="text-xl font-black text-gray-50">M</p>
+    </div>
+  );
+};
+export default Logo;
